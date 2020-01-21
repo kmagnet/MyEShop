021482e988449c4f707e1a5164c22d198c074c03
Karlo Magno Guadlaupe <karloguadalupe@gmail.com>
2020-01-21 20:33:21 +1300

    Initial Solution Setup

---
+++ MyEShop/MyEShop.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyEShop.WebUI.MvcApplication" Language="C#" %>
---
+++ MyEShop/MyEShop.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyEShop.WebUI.Startup))]
+namespace MyEShop.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
