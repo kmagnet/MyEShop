cfb0a887ed5eb29ac3245c10b2b75acf92db44e1
Karlo Magno Guadlaupe <karloguadalupe@gmail.com>
2020-01-23 16:56:56 +1300

    Added Entity Framework

---
+++ MyEShop/MyEShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using MyEShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEShop.DataAccess.SQL
+{
+    public class DataContext :DbContext
+    {
+        public DataContext()
+            : base("DefaultCOnnection") { 
+
+            /// Tells DataContext class to look into the Web.config (or into App.config) file and look for section called DefaultConnection
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
---
+++ MyEShop/MyEShop.DataAccess.SQL/Migrations/202001230309293_Initial.cs
@@ -0,0 +1,45 @@
+﻿namespace MyEShop.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        CategoryName = c.String(),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(maxLength: 20),
+                        Description = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Category = c.String(),
+                        Image = c.String(),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Products");
+            DropTable("dbo.Categories");
+        }
+    }
+}
