1acbf4910a53c7fc4779bd18582637db5f9fc5c8
Karlo Magno Guadlaupe <karloguadalupe@gmail.com>
2020-01-23 23:32:23 +1300

    Added Product Listing Page

--- MyEShop/MyEShop.WebUI/Controllers/HomeController.cs
+++ MyEShop/MyEShop.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MyEShop.Core.Contracts;
+using MyEShop.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,36 @@ namespace MyEShop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        IContainer<Product> context;
+        IContainer<Category> categories;
+
+        public HomeController(IContainer<Product> productContext, IContainer<Category> categoryContext)
+        {
+            // Product Manager Controller constructor to initialize
+            // concrete implementation of product container and category IContainers 
+            // then stores them into their respective context variables
+
+            context = productContext;
+            categories = categoryContext;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            List<Product> products = context.Container().ToList();
+            return View(products);
+        }
+
+        public ActionResult ViewProductDetails(string Id) {
+            
+            Product product = context.Search(Id);
+
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else {
+                return View(product);
+            }
         }
 
         public ActionResult About()
